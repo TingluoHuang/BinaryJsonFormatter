02d7e62ac27b709405d327e3442d18059356cfdf
Tingluo Huang <tingluohuang@github.com>
2020-06-13 18:19:56 +0000

    fix

--- src/components/JSONFormatter/JSONFormatter.jsx
+++ src/components/JSONFormatter/JSONFormatter.jsx
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-import Button from '@material-ui/core/Button';
 import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
 import { UnControlled as CodeMirror } from 'react-codemirror2'
 
